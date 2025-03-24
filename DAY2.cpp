b1544eb78602e56e4bd09d1b880ddc3605f7f2b6
Krrish-29 <kingkrrish29@gmail.com>
2025-03-24 20:48:11 +0530

    DAY 2 Solved #14

--- DAY2.cpp
+++ DAY2.cpp
@@ -15,4 +15,22 @@ class Solution {
     
             return answer;
         }
+        // string longestCommonPrefix(vector<string>& strs) {
+        //     for(int i=1;i<strs.size();i++){
+        //         if(strs[i].length()==0){
+        //             strs[0]="";
+        //             break;
+        //         }
+        //         if(strs[i].size()<strs[0].size()){
+        //             strs[0].erase(strs[i].size(),strs[0].size()-strs[i].size());
+        //         }
+        //         for(int j=0;j<strs[i].size()&&j<strs[0].size();j++){
+        //             if(strs[i][j]!=strs[0][j]){
+        //             strs[0].erase(j);
+        //             break;
+        //             }
+        //         }
+        //     }
+        //     return strs[0];
+        // }
     };  
