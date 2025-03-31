97421955543582f5c11ac465d0a9d27d6072482e
Krrish-29 <kingkrrish29@gmail.com>
2025-03-31 12:37:20 +0530

    DAY 9 Solved #26

--- DAY9.cpp
+++ DAY9.cpp
@@ -1,22 +1,22 @@
 // 26. Remove Duplicates from Sorted Array
 
-// class Solution {
-//     public:
-//         int removeDuplicates(vector<int>& nums) {
-//             int answer=1,x=nums[0];
-//             for(int i=1;i<nums.size();i++){
-//                 if(x==nums[i]){
-//                     nums.erase(nums.begin()+i);
-//                     i--;
-//                 }
-//                 else{
-//                     x=nums[i];
-//                     answer++;
-//                 }
-//             }
-//             return answer;
-//         }
-//     };
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int answer=1,x=nums[0];
+            for(int i=1;i<nums.size();i++){
+                if(x==nums[i]){
+                    nums.erase(nums.begin()+i);
+                    i--;
+                }
+                else{
+                    x=nums[i];
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    };
 
 // class Solution {
 //     public:
