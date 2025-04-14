1fce7a562445252f566e8d54d7f47fd96cae8bc6
Krrish-29 <kingkrrish29@gmail.com>
2025-04-14 13:17:36 +0530

    DAY23 14/4/25 Solved #1534

---
+++ DAY0023.cpp
@@ -0,0 +1,15 @@
+// 1534. Count Good Triplets
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int goodTriplet=0,size=arr.size();
+            for(int i=0;i<size-2;i++){
+                for(int j=i+1;j<size-1;j++){
+                    for(int k=j+1;k<size;k++){
+                        if((abs(arr[i]-arr[j])<=a)&&(abs(arr[j]-arr[k])<=b)&&(abs(arr[i]-arr[k])<=c)) goodTriplet++;
+                    }
+                }
+            }
+            return goodTriplet;
+        }
+    };
