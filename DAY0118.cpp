18676849230811c6aa7f199f8169f49cbfe082df
Krrish-29 <kingkrrish29@gmail.com>
2025-07-18 23:11:22 +0530

    DAY118 18/7/25 Solved #2163

---
+++ DAY0118.cpp
@@ -0,0 +1,68 @@
+// 2163. Minimum Difference in Sums After Removal of Elements
+class Solution {
+public:
+    long long minimumDifference(vector<int>& nums) {
+        priority_queue<int,vector<int>>maxheap;
+        priority_queue<int,vector<int>,greater<int>>minheap;
+        int n=nums.size()/3,leftsum=0,rightsum=0;
+        vector<int>leftminsum(2*n,0);
+        vector<int>rightmaxsum(2*n,0);
+        for(int i=0;i<2*n;i++){
+            maxheap.push(nums[i]);
+            leftsum+=nums[i];
+            if(maxheap.size()>n){
+                leftsum-=maxheap.top();
+                maxheap.pop();
+            }
+            leftminsum[i]=leftsum;
+        }
+        for(int i=3*n-1;i>=n;i--){
+            minheap.push(nums[i]);
+            rightsum+=nums[i];
+            if(minheap.size()>n){
+                rightsum-=minheap.top();
+                minheap.pop();
+            }
+            rightmaxsum[i-n]=rightsum;
+        }
+        long long res=LLONG_MAX;
+        for (int i=n-1;i<2*n;i++){
+            res = min(res,(long long)(leftminsum[i]-rightmaxsum[i-n+1]));
+        }
+        return res;
+    }
+};
+        // priority_queue<int,vector<int>>maxheap;
+        // priority_queue<int,vector<int>,greater<int>>minheap;
+        // int n=nums.size()/3;
+        // int res=INT_MAX;
+        // for(int j=n;j<2*n;j++){
+        //     int i=0;
+        //     while(i<=j){
+        //         minheap.push(nums[i]);
+        //         i++;
+        //     }
+        //     int k=j+1;
+        //     while(k<3*n){
+        //         maxheap.push(nums[k]);
+        //         k++;
+        //     }
+        //     int min_sum=0;
+        //     int max_sum=0;
+        //     int length=n;
+        //     while(!minheap.empty()&&length>=0){
+        //         min_sum+=minheap.top();
+        //         minheap.pop();
+        //         length--;
+        //     }
+        //     length=n;
+        //     while(!maxheap.empty()&&length>=0){
+        //         max_sum+=maxheap.top();
+        //         maxheap.pop();
+        //         length--;
+        //     }
+        //     res=min(res,min_sum-max_sum);
+        //     while(!minheap.empty())minheap.pop();
+        //     while(!maxheap.empty())maxheap.pop();
+        // }
+        // return res;
