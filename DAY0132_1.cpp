0801c41049c4c96c437b49873402aaebdf060c42
Krrish-29 <kingkrrish29@gmail.com>
2025-08-01 23:46:00 +0530

    DAY132 32/7/25 Solved #118

---
+++ DAY0132_1.cpp
@@ -0,0 +1,14 @@
+// 118. Pascal's Triangle
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>triangle(numRows);
+        for(int i=0;i<numRows;i++){
+            triangle[i]=vector<int>(i+1,1);
+            for(int j=1;j<i;j++){
+                triangle[i][j]=(triangle[i-1][j]+triangle[i-1][j-1]);
+            }
+        }
+        return triangle;
+    }
+};
