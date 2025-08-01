a4643f273265bcef03eb660e4829d58197e5f33c
Krrish-29 <kingkrrish29@gmail.com>
2025-08-01 23:46:36 +0530

    DAY132 32/7/25 Solved #58

---
+++ DAY0132_2.cpp
@@ -0,0 +1,16 @@
+// 58. Length of Last Word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int size=s.size(),len=0;
+        bool firstLetter=false;
+        for(int i=size-1;i>=0;i--){
+            if(s[i]==' '&&firstLetter) break;
+            else if(s[i]!=' '){
+                len++; 
+                firstLetter=true;
+            }
+        }
+        return len;
+    }
+};
