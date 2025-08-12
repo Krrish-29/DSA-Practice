aca7e29cce52823d07412052405d19e3311b394f
Krrish-29 <kingkrrish29@gmail.com>
2025-08-13 00:22:15 +0530

    DAY143 12/8/25 Solved #2810

---
+++ DAY0143_1.cpp
@@ -0,0 +1,35 @@
+// 500. Keyboard Row
+class Solution {
+public:
+    bool checkRow(string s){
+        int size=s.size();
+        string row1="QWERTYUIOPqwertyuiop",row2="ASDFGHJKLasdfghjkl",row3="ZXCVBNMzxcvbnm";
+        if(row1.contains(s[0])){
+            for(int i=1;i<size;i++){
+                if(!row1.contains(s[i])) return false;
+            }
+        }
+        else if(row2.contains(s[0])){
+            for(int i=1;i<size;i++){
+                if(!row2.contains(s[i])) return false;
+            }
+        }
+        else if(row3.contains(s[0])){
+            for(int i=1;i<size;i++){
+                if(!row3.contains(s[i])) return false;
+            }
+        }
+        return true;
+    }
+    vector<string> findWords(vector<string>& words) {
+        int size=words.size();
+        for(int i=0;i<size;i++){
+            if(!checkRow(words[i])){
+                words.erase(words.begin()+i);
+                i--;
+                size--;
+            }
+        }
+        return words;
+    }
+};
