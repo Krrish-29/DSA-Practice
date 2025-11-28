d778f4009df06ee4a487193634b4f21e96070ddb
Krrish-29 <kingkrrish29@gmail.com>
2025-11-28 22:55:47 +0530

    DAY251 28/11/25 Solved #2872

---
+++ DAY0251.cpp
@@ -0,0 +1,25 @@
+// 2872. Maximum Number of K-Divisible Components
+class Solution {
+private:
+    vector<vector<int>>adj_list;
+    int count=0;
+    long long calculate(int index,vector<int>& values,int & k,int parent){
+        long long sum=values[index];
+        for(int idx:adj_list[index]) if(parent!=idx) sum+=calculate(idx,values,k,index);
+        if(sum%k==0){
+            count++;
+            return 0;
+        }
+        return sum;
+    }
+public:
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        adj_list.resize(n);
+        for(auto edge:edges){
+            adj_list[edge[0]].push_back(edge[1]);
+            adj_list[edge[1]].push_back(edge[0]);
+        }
+        calculate(0,values,k,-1);
+        return count;
+    }
+};
