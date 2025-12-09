68c636b8983c67b389e0c032018abcf64e22e525
Krrish-29 <kingkrrish29@gmail.com>
2025-12-09 12:46:56 +0530

    DAY262 9/12/25 Solved #3538

---
+++ DAY0262.cpp
@@ -0,0 +1,16 @@
+// 3583. Count Special Triplets
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int MOD=1e9+7;
+        unordered_map<int,int>prefix,suffix;
+        int count=0,size=nums.size();
+        for(int num:nums) prefix[num]++;
+        for(int i=0;i<size;i++){
+            prefix[nums[i]]--;
+            count=(count+(suffix[nums[i]*2]*1LL*prefix[nums[i]*2])%MOD)%MOD;
+            suffix[nums[i]]++;
+        }
+        return count;
+    }
+};
