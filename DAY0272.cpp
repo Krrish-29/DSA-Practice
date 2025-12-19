65e0f4890e63396a0a9f54d70eb499d39d3f8722
Krrish-29 <kingkrrish29@gmail.com>
2025-12-19 08:42:20 +0530

    DAY272 19/12/25 Solved #33

---
+++ DAY0272.cpp
@@ -0,0 +1,31 @@
+// 33. Search in Rotated Sorted Array
+class Solution {
+private:
+    int pivot(vector<int>&nums,int size){
+        int left=0,right=size-1,mid;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if(nums[mid]>nums[right]) left=mid+1;
+            else right=mid;
+        }
+        return left;
+    }
+    int binary_search(vector<int>&nums,int left,int right,int target){
+        int mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        int size=nums.size();
+        int k=pivot(nums,size);
+        int ans=binary_search(nums,0,k-1,target);
+        if(ans==-1) ans=binary_search(nums,k,size-1,target);
+        return ans;
+    }
+};
