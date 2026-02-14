77d8e36447d53a2b53d841c63751a1c12478c223
Krrish-29 <kingkrrish29@gmail.com>
2026-02-14 23:48:22 +0530

    DAY329 14/2/26 Solved #3838

---
+++ DAY0329_1.cpp
@@ -0,0 +1,15 @@
+// 3838. Weighted Word Mapping
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        for(string &str:words){
+            int sum=0;
+            for(char &ch:str){
+                sum+=weights[ch-'a'];
+            }
+            ans+='a'+(25-(sum%26));
+        }
+        return ans;
+    }
+};
