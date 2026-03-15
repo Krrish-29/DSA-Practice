dd46f0a17972698919f8ecb113f84d34324cfb53
Krrish-29 <kingkrrish29@gmail.com>
2026-03-15 23:18:18 +0530

    DAY357 14/3/26 Solved #3870

---
+++ DAY0358_1.cpp
@@ -0,0 +1,11 @@
+// 3870. Count Commas in Range
+class Solution {
+public:
+    int countCommas(int n) {
+        string num=to_string(n);
+        if(num.size()<=3) return 0;
+        int comma=0;
+        if(n>999) comma+=n-999;
+        return comma;
+    }
+};
