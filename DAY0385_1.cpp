436947dde44c1c323aca4fcdfa0d206b9c62f555
Krrish-29 <kingkrrish29@gmail.com>
2026-04-11 23:39:37 +0530

    DAY385 11/4/26 Solved #3894

---
+++ DAY0385_1.cpp
@@ -0,0 +1,10 @@
+// 3894. Traffic Signal Color
+class Solution {
+public:
+    string trafficSignal(int timer) {
+        if(timer==0) return "Green";
+        if(timer==30) return "Orange";
+        if(timer>30&&timer<=90) return "Red";
+        return "Invalid";
+    }
+};
