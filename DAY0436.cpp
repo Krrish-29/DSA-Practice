ff3a532ecac6ac82ae386fac9f443fe70f87e6b7
Krrish-29 <kingkrrish29@gmail.com>
2026-06-02 01:44:03 +0530

    DAY436 1/6/26 Solved #3945

---
+++ DAY0436.cpp
@@ -0,0 +1,16 @@
+// 3945. Digit Frequency Score
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        vector<int>freq(10,0);
+        while(n>0){
+            freq[n%10]++;
+            n/=10;
+        }
+        int sum=0;
+        for(int i=0;i<10;i++){
+            sum+=i*freq[i];
+        }
+        return sum;
+    }
+};
