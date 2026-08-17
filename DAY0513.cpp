a0bdafe25f2194de31c02c179754e4a6251eb236
Krrish-29 <kingkrrish29@gmail.com>
2026-08-18 01:42:17 +0530

    DAY513 17/8/26 Solved #191

---
+++ DAY0513.cpp
@@ -0,0 +1,11 @@
+// 191. Number of 1 Bits
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        for(int i=0;i<=31;i++){
+            if((n&(1<<i))!=0) count++;
+        }
+        return count;
+    }
+};
